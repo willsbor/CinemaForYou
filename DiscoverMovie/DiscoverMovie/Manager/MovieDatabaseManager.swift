738a2ea4d843081f2c4b0626a3c30be22262aaf5
willsbor Kang <willsbor@gmail.com>
2019-03-17 11:42:53 +0800

    initial MovieDatabaseManager

--- DiscoverMovie/DiscoverMovie/Manager/MovieDatabaseManager.swift
+++ DiscoverMovie/DiscoverMovie/Manager/MovieDatabaseManager.swift
@@ -10,4 +10,190 @@ import Foundation
 
 class MovieDatabaseManager {
     
+    enum Errors: Error {
+        case createURLComponentsFailed
+        case createRequestFailed
+        case statusCodeInvalid
+        case undefinedStatus(Int)
+        case dataInNil
+    }
+    
+    enum Result {
+        case success(DiscoverResult)
+        case status401(statusCode: Int, statusMessage: String)
+        case status404(statusCode: Int, statusMessage: String)
+        case failed(Error)
+    }
+    
+    enum SortType: String {
+        case popularityASC = "popularity.asc"
+        case popularityDESC = "popularity.desc"
+        case releaseDateASC = "release_date.asc"
+        case releaseDateDESC = "release_date.desc"
+    }
+    
+    struct DiscoverResult: Codable, Equatable {
+        var page: Int
+        var totalResults: Int
+        var totalPages: Int
+        var results: [MovieData]
+        
+        enum CodingKeys: String, CodingKey {
+            case page
+            case totalResults = "total_results"
+            case totalPages = "total_pages"
+            case results
+        }
+    }
+    
+    struct MovieData: Codable, Equatable {
+        var id: Int
+        var video: Bool
+        var title: String
+        var popularity: Double
+        var posterPath: String?
+        var originalLanguage: String
+        var originalTitle: String
+        var genreIDs: [Int]
+        var adult: Bool
+        var overview: String
+        var releaseDate: String
+        var backdropPath: String?
+        var voteAverage: Double
+        var voteCount: Int
+        
+        enum CodingKeys: String, CodingKey {
+            case id
+            case video
+            case title
+            case popularity
+            case posterPath = "poster_path"
+            case originalLanguage = "original_language"
+            case originalTitle = "original_title"
+            case genreIDs = "genre_ids"
+            case adult
+            case overview
+            case releaseDate = "release_date"
+            case backdropPath = "backdrop_path"
+            case voteAverage = "vote_average"
+            case voteCount = "vote_count"
+        }
+    }
+    
+    struct FailedResult: Decodable, Equatable {
+        var statusMessage: String
+        var statusCode: Int
+        
+        enum CodingKeys: String, CodingKey {
+            case statusMessage = "status_message"
+            case statusCode = "status_code"
+        }
+    }
+    
+    private enum ServiceEndPoint: String {
+        case discoverMovie = "discover/movie"
+    }
+    
+    let appKey = "0015d20b3f7005f8aec145f45a71ad00"
+    private let baseURL = URL(string: "https://api.themoviedb.org")!
+    private let serviceVersion = "3"
+    private let urlSession: URLSession
+    
+    init(_ sessionConfiguration: URLSessionConfiguration = .default) {
+        urlSession = URLSession(configuration: sessionConfiguration)
+    }
+    
+    func discoverMovies(_ sort: SortType, _ page: Int, _ lang: String, _ region: String, _ completionHandler: @escaping (Result) -> Void) {
+        
+        assert(region.isMatch("^[A-Z]{2}$"), "region format is invalid. (spec: https://developers.themoviedb.org/3/discover/movie-discover)")
+        assert(lang.isMatch("^[a-z]{2}-[A-Z]{2}$"), "lang format is invalid. (spec: https://developers.themoviedb.org/3/discover/movie-discover)")
+        
+        guard var components = URLComponents(url: makeURL(.discoverMovie), resolvingAgainstBaseURL: false) else {
+            DispatchQueue.global().async {
+                completionHandler(.failed(Errors.createURLComponentsFailed))
+            }
+            return
+        }
+        components.queryItems = [
+            URLQueryItem(name: "sort_by", value: sort.rawValue),
+            URLQueryItem(name: "page", value: "\(page)"),
+            URLQueryItem(name: "language", value: lang),
+            URLQueryItem(name: "region", value: region),
+            URLQueryItem(name: "api_key", value: appKey),
+        ]
+        
+        guard let url = components.url else {
+            DispatchQueue.global().async {
+                completionHandler(.failed(Errors.createRequestFailed))
+            }
+            return
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let task = urlSession.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                completionHandler(.failed(error))
+                return
+            }
+            
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode else {
+                completionHandler(.failed(Errors.statusCodeInvalid))
+                return
+            }
+            
+            do {
+                switch statusCode {
+                case 200:
+                    guard let result = try data?.toDiscoverResult() else {
+                        throw Errors.dataInNil
+                    }
+                    completionHandler(.success(result))
+                    
+                case 401:
+                    guard let (code, message) = try data?.toStatusCodeAndMessage() else {
+                        throw Errors.dataInNil
+                    }
+                    completionHandler(.status401(statusCode: code, statusMessage: message))
+                    
+                case 404:
+                    guard let (code, message) = try data?.toStatusCodeAndMessage() else {
+                        throw Errors.dataInNil
+                    }
+                    completionHandler(.status404(statusCode: code, statusMessage: message))
+                    
+                default:
+                    completionHandler(.failed(Errors.undefinedStatus(statusCode)))
+                }
+            } catch {
+                completionHandler(.failed(error))
+            }
+        }
+        
+        task.resume()
+    }
+    
+    private func makeURL(_ endPoint: ServiceEndPoint) -> URL {
+        return baseURL.appendingPathComponent(serviceVersion).appendingPathComponent(endPoint.rawValue)
+    }
+}
+
+extension Data {
+    fileprivate func toDiscoverResult() throws -> MovieDatabaseManager.DiscoverResult {
+        let decoder = JSONDecoder()
+        return try decoder.decode(MovieDatabaseManager.DiscoverResult.self, from: self)
+    }
+    
+    fileprivate func toStatusCodeAndMessage() throws -> (Int, String) {
+        let decoder = JSONDecoder()
+        let object = try decoder.decode(MovieDatabaseManager.FailedResult.self, from: self)
+        return (object.statusCode, object.statusMessage)
+    }
+}
+
+extension String {
+    fileprivate func isMatch(_ pattern: String) -> Bool {
+        let re = try! NSRegularExpression(pattern: pattern)
+        return re.firstMatch(in: self, range: NSRange(self.startIndex..., in: self)) != nil
+    }
 }
---
+++ DiscoverMovie/DiscoverMovieTests/MovieDatabaseManagerTests.swift
@@ -0,0 +1,175 @@
+//
+//  MovieDatabaseManagerTests.swift
+//  DiscoverMovieTests
+//
+//  Created by willsborKang on 2019/3/17.
+//  Copyright © 2019 willsborKang. All rights reserved.
+//
+
+import XCTest
+
+class MovieDatabaseManagerTests: XCTestCase {
+
+    class MockURLProtocol: URLProtocol {
+        
+        static var responseStatusCode = 200
+        static var responseBody: Data?
+        static var inputRequest: URLRequest?
+        
+        override class func canInit(with request: URLRequest) -> Bool {
+            return true
+        }
+        
+        override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+            MockURLProtocol.inputRequest = request
+            return request
+        }
+        
+        override func startLoading() {
+            print("startLoading ...")
+            DispatchQueue.global().async {
+                let response = HTTPURLResponse(url: self.request.url!, statusCode: MockURLProtocol.responseStatusCode, httpVersion: "1.1", headerFields: nil)!
+                
+                self.client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+                if let data = MockURLProtocol.responseBody {
+                    self.client?.urlProtocol(self, didLoad: data)
+                }
+                self.client?.urlProtocolDidFinishLoading(self)
+            }
+        }
+        
+        override func stopLoading() {
+            print("stopLoading ...")
+        }
+    }
+    
+    var movieDatabaseManager: MovieDatabaseManager!
+    
+    override func setUp() {
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [MockURLProtocol.self]
+        movieDatabaseManager = MovieDatabaseManager(configuration)
+    }
+
+    override func tearDown() {
+        movieDatabaseManager = nil
+    }
+
+    func testDiscoverMoviesWith200() {
+        
+        /// Given
+        let (answer, responseBody): (MovieDatabaseManager.DiscoverResult, Data) = makeFakeDiscoverResult("DiscoverResult200-1")
+        MockURLProtocol.responseBody = responseBody
+        MockURLProtocol.responseStatusCode = 200
+        let page = 3
+        let sort = MovieDatabaseManager.SortType.releaseDateDESC
+        let region = "TW"
+        let lang = "zh-TW"
+        
+        /// When
+        let expect = expectation(description: "wait for discovery")
+        var result: MovieDatabaseManager.Result!
+        movieDatabaseManager.discoverMovies(sort, page, lang, region) { (r) in
+            result = r
+            expect.fulfill()
+        }
+        wait(for: [expect], timeout: 3)
+        
+        /// Then
+        assertInputDiscoverRequest(sort, page, lang, region)
+        
+        switch result! {
+        case .success(let data):
+            XCTAssertEqual(data, answer)
+            XCTAssertEqual(data.page, page)
+        case .status401, .status404, .failed:
+            XCTFail()
+        }
+    }
+    
+    func testDiscoverMoviesWith401() {
+        
+        /// Given
+        let (answer, responseBody): (MovieDatabaseManager.FailedResult, Data) = makeFakeDiscoverResult("DiscoverResult401-1")
+        MockURLProtocol.responseBody = responseBody
+        MockURLProtocol.responseStatusCode = 401
+        let page = 3
+        let sort = MovieDatabaseManager.SortType.releaseDateDESC
+        let region = "TW"
+        let lang = "zh-TW"
+        
+        /// When
+        let expect = expectation(description: "wait for discovery")
+        var result: MovieDatabaseManager.Result!
+        movieDatabaseManager.discoverMovies(sort, page, lang, region) { (r) in
+            result = r
+            expect.fulfill()
+        }
+        wait(for: [expect], timeout: 3)
+        
+        /// Then
+        assertInputDiscoverRequest(sort, page, lang, region)
+        
+        switch result! {
+        case .status401(let statusCode, let statusMessage):
+            XCTAssertEqual(statusCode, answer.statusCode)
+            XCTAssertEqual(statusMessage, answer.statusMessage)
+        case .success, .status404, .failed:
+            XCTFail()
+        }
+    }
+    
+    func testDiscoverMoviesWith404() {
+        
+        /// Given
+        let (answer, responseBody): (MovieDatabaseManager.FailedResult, Data) = makeFakeDiscoverResult("DiscoverResult404-1")
+        MockURLProtocol.responseBody = responseBody
+        MockURLProtocol.responseStatusCode = 404
+        let page = 3
+        let sort = MovieDatabaseManager.SortType.releaseDateDESC
+        let region = "TW"
+        let lang = "zh-TW"
+        
+        /// When
+        let expect = expectation(description: "wait for discovery")
+        var result: MovieDatabaseManager.Result!
+        movieDatabaseManager.discoverMovies(sort, page, lang, region) { (r) in
+            result = r
+            expect.fulfill()
+        }
+        wait(for: [expect], timeout: 3)
+        
+        /// Then
+        assertInputDiscoverRequest(sort, page, lang, region)
+        
+        switch result! {
+        case .status404(let statusCode, let statusMessage):
+            XCTAssertEqual(statusCode, answer.statusCode)
+            XCTAssertEqual(statusMessage, answer.statusMessage)
+        case .success, .status401, .failed:
+            XCTFail()
+        }
+    }
+    
+    private func assertInputDiscoverRequest(_ sort: MovieDatabaseManager.SortType, _ page: Int, _ lang: String, _ region: String) {
+        XCTAssertEqual(MockURLProtocol.inputRequest!.httpMethod, "GET")
+        let resultComponent = URLComponents(url: MockURLProtocol.inputRequest!.url!, resolvingAgainstBaseURL: false)!
+        XCTAssertEqual(resultComponent.queryItems!.count, 5)
+        XCTAssertTrue(resultComponent.queryItems!.contains(where: { $0.name == "sort_by" && $0.value == sort.rawValue }))
+        XCTAssertTrue(resultComponent.queryItems!.contains(where: { $0.name == "page" && $0.value == "\(page)" }))
+        XCTAssertTrue(resultComponent.queryItems!.contains(where: { $0.name == "language" && $0.value == "\(lang)" }))
+        XCTAssertTrue(resultComponent.queryItems!.contains(where: { $0.name == "region" && $0.value == "\(region)" }))
+        XCTAssertTrue(resultComponent.queryItems!.contains(where: { $0.name == "api_key" && $0.value == movieDatabaseManager.appKey }))
+    }
+    
+    private func makeFakeDiscoverResult<T: Decodable>(_ fileName: String) -> (T, Data) {
+        let bundle = Bundle(for: type(of: self))
+        let path = bundle.path(forResource: fileName, ofType: "json")!
+        let jsonData = try! Data(contentsOf: URL(fileURLWithPath: path))
+        
+        let decoder = JSONDecoder()
+        let object = try! decoder.decode(T.self, from: jsonData)
+
+        return (object, jsonData)
+    }
+}
